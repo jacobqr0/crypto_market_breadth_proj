5afc2d0c6f6d09131c61c434c5c80e64e381b11d
jacobqr0 <jacobrankin73@hotmail.com>
2024-01-06 17:56:04 -0600

    Added parameter configurations and data classes

--- source/coingecko_api.py
+++ source/coingecko_api.py
@@ -19,6 +19,26 @@ class CoinGeckoMarketChartState:
 class CoinGeckoState:
     api: Optional[str]
     marketchart_state: Optional[CoinGeckoMarketChartState]
+    last_query_time: Optional[str]
+
+@dataclass 
+class CoinMarketParameters:
+    vs_currency: str
+    order: str
+    per_page: str
+
+@dataclass 
+class MarketChartParameters:
+    vs_currency: str
+
+@dataclass 
+class APIBaseParameters:
+    coinmarkets: CoinMarketParameters
+    marketchart: MarketChartParameters
+
+@dataclass
+class CoinGeckoSecrets:
+    base_url: str
 
 class CoinGeckoAPI:
 
@@ -30,9 +50,30 @@ class CoinGeckoAPI:
         API_KEY_MARKET_CHART: CoinGeckoEndpoint(API_KEY_MARKET_CHART, "marketchart")
     }
 
-    def __init__(self, state: dict) -> None:
-        self.parse_request(state)
+    def __init__(self, state: dict, secrets: dict) -> None:
+        self.parse_request(state, secrets)
 
-    def parse_request(self, state: dict):
+    def parse_request(self, state: dict, secrets: dict):
         self.original_state: CoinGeckoState = from_dict(data_class=CoinGeckoState, data=state)
         self.updated_state: dict = asdict(self.original_state)
+        self.secrets: CoinGeckoSecrets = from_dict(data_class=CoinGeckoSecrets, data=secrets)
+
+    def build_api(self) -> str:
+        api = self.API_ENDPOINTS[self.API_KEY_COIN_MARKETS] if self.original_state.api is None else self.API_ENDPOINTS[self.original_state.api]
+
+        # TODO - build default parameters for coin markets endpoint
+        # Parameters for Coin Markets endpoint will not be configurable 
+
+        if api.api == self.API_KEY_COIN_MARKETS:
+            pass
+        
+        elif api.api == self.API_KEY_MARKET_CHART:
+            if self.original_state.marketchart_state is None:
+                existing_asset_id = None 
+            else:
+                existing_asset_id = self.original_state.marketchart_state.asset_id
+        
+
+
+
+
