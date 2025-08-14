2562f20f29ae5e173b707d05060470cccf7f0ea5
jacobqr0 <jacobrankin73@hotmail.com>
2025-08-14 18:44:33 -0500

    Updated files

--- source/coingecko_api.py
+++ source/coingecko_api.py
@@ -1,6 +1,7 @@
 from dataclasses import asdict, dataclass
 from dacite import from_dict
-from typing import Optional
+from typing import Optional, Dict, List
+from datetime import datetime, timedelta
 
 import requests 
 import json
@@ -13,13 +14,13 @@ class CoinGeckoEndpoint:
 
 @dataclass
 class CoinGeckoMarketChartState:
-    asset_id: Optional[list[str]]
+    already_fetched: Dict[str, Dict]
+    to_query_asset_id: Optional[List[str]]
 
 @dataclass
 class CoinGeckoState:
     api: Optional[str]
-    marketchart_state: Optional[CoinGeckoMarketChartState]
-    last_query_time: Optional[str]
+    marketchart_state: CoinGeckoMarketChartState
 
 @dataclass 
 class CoinMarketParameters:
@@ -30,6 +31,8 @@ class CoinMarketParameters:
 @dataclass 
 class MarketChartParameters:
     vs_currency: str
+    initial_query_from: int
+    query_to: int
 
 @dataclass 
 class APIBaseParameters:
@@ -39,6 +42,7 @@ class APIBaseParameters:
 @dataclass
 class CoinGeckoSecrets:
     base_url: str
+    parameters: APIBaseParameters
 
 class CoinGeckoAPI:
 
@@ -61,14 +65,31 @@ class CoinGeckoAPI:
     def build_api(self) -> str:
         api = self.API_ENDPOINTS[self.API_KEY_COIN_MARKETS] if self.original_state.api is None else self.API_ENDPOINTS[self.original_state.api]
 
-        # TODO - build default parameters for coin markets endpoint
-        # Parameters for Coin Markets endpoint will not be configurable 
-
         if api.api == self.API_KEY_COIN_MARKETS:
-            pass
+
+            # TODO: Figure out if we need to paginate 
+            api_endpoint = (f"{self.secrets.base_url}"
+                            f"coins/markets?vs_currency={self.secrets.parameters.coinmarkets.vs_currency}"
+                            f"&order={self.secrets.parameters.coinmarkets.order}"
+                            f"&per_page={self.secrets.parameters.coinmarkets.per_page}"
+                            f"&page=1"
+                            )
         
         elif api.api == self.API_KEY_MARKET_CHART:
-            if self.original_state.marketchart_state is None:
-                existing_asset_id = None 
+            self.current_asset_id = self.original_state.marketchart_state.to_query_asset_id[0]
+            query_to = self.secrets.parameters.marketchart.query_to
+            
+            if self.current_asset_id in self.original_state.marketchart_state.already_fetched.keys():
+                query_from = self.original_state.marketchart_state.already_fetched[self.current_asset_id]["last_fetched"]
+            
             else:
-                existing_asset_id = self.original_state.marketchart_state.asset_id
+                query_from = self.secrets.parameters.marketchart.initial_query_from
+
+            api_endpoint = (f"{self.secrets.base_url}"
+                            f"coins/{self.current_asset_id}/market_chart/range"
+                            f"?vs_currency={self.secrets.parameters.marketchart.vs_currency}"
+                            f"&from={query_from}"
+                            f"&to={query_to}"
+                            )
+
+        return api_endpoint
--- source/test_coingecko_api.py
+++ source/test_coingecko_api.py
@@ -1,21 +1,78 @@
 import pytest 
-import coingecko_api
-
-BASIC_SECRETS = {
-    'base_url': 'https://api.coingecko.com/api/v3/'
-}
+from coingecko_api import CoinGeckoAPI
+from typing import Optional, List, Dict
 
 BASIC_PARAMETERS = {
     "coinmarkets": {
         "vs_currency": "usd",
         "order": "market_cap_des",
-        "per_page": "300"
+        "per_page": "250"
     },
 
     "marketchart": {
-        "vs_currency": "usd"
+        "vs_currency": "usd",
+        "initial_query_from": 1739340000,
+        "query_to": 1754888400
     }
 }
 
-BASIC_STATE = {}
+BASIC_SECRETS = {
+    "base_url": "https://api.coingecko.com/api/v3/",
+    "parameters": BASIC_PARAMETERS
+}
 
+BASIC_STATE = {
+    "marketchart_state": {
+        "already_fetched": {}
+    }
+}
+
+BASIC_MARKETCHART_STATE = {
+    "already_fetched": {"bitcoin": {"last_fetched": 1739340000}, "ethereum": {"last_fetched": 1739340000}},
+    "to_query_asset_id": ["bitcoin", "ethereum"]
+}
+
+def test_parse_request():
+
+    api = CoinGeckoAPI(BASIC_STATE, BASIC_SECRETS)
+
+    assert api.secrets.base_url == "https://api.coingecko.com/api/v3/"
+    assert api.secrets.parameters.coinmarkets.vs_currency == "usd"
+    assert api.secrets.parameters.coinmarkets.order == "market_cap_des"
+    assert api.secrets.parameters.coinmarkets.per_page == "250"
+    assert api.secrets.parameters.marketchart.vs_currency == "usd"
+    assert api.original_state.marketchart_state.already_fetched == {}
+
+def test_parse_request_nonblank_api():
+
+    api_state = {
+        "api": "marketchart",
+        "marketchart_state": {
+            "already_fetched": {}
+        }
+        }
+
+    api = CoinGeckoAPI(api_state, BASIC_SECRETS)
+
+    assert api.original_state.api == "marketchart"
+
+@pytest.mark.parametrize("input_api, assets, already_fetched, expected_endpoint", [
+    (None, None, {}, "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_des&per_page=250&page=1"),
+    ("marketchart", ["bitcoin", "ethereum"], {}, "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1739340000&to=1754888400"),
+    ("marketchart", ["bitcoin", "ethereum"], {"bitcoin": {"last_fetched": 1739349999}, "ethereum": {"last_fetched": 1739340000}}, "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1739349999&to=1754888400"),
+])
+def test_build_api_multiple_states(input_api: Optional[str], assets: Optional[List], already_fetched: Optional[Dict], expected_endpoint: str):
+
+    state = {
+
+        "api": input_api,
+        "marketchart_state": {
+            "to_query_asset_id": assets,
+            "already_fetched": already_fetched
+            }
+        }
+    
+    api = CoinGeckoAPI(state, BASIC_SECRETS)
+    actual_endpoint = api.build_api()
+    
+    assert actual_endpoint == expected_endpoint
